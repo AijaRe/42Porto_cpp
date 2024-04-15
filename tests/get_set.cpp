d42324e46837b2470b716846cd617c7170867b51
AijaRe <aija.repsa@gmail.com>
2024-04-15 20:02:57 +0100

    first file and gitignore

---
+++ tests/get_set.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class Employee {
+  private:
+    // Private attribute
+    int salary;
+
+  public:
+    // Setter
+    void setSalary(int s) {
+      salary = s;
+    }
+    // Getter
+    int getSalary() {
+      return salary;
+    }
+};
+
+int main() {
+  Employee myObj;
+  myObj.setSalary(50000);
+  std::cout << myObj.getSalary() << std::endl;;
+  return 0;
+} 
