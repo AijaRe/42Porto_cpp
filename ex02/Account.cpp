dd56ae76c9669efb199904f722243fadc546c51a
AijaRe <aija.repsa@gmail.com>
2024-04-25 16:59:23 +0100

    started account.cpp

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: arepsa <arepsa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 16:19:39 by arepsa            #+#    #+#             */
-/*   Updated: 2024/04/25 16:24:12 by arepsa           ###   ########.fr       */
+/*   Updated: 2024/04/25 16:49:23 by arepsa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,30 @@ int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
-int	Account::getNbAccounts(void)
-{
+int	Account::getNbAccounts(void){
 	return (_nbAccounts);
 }
 
-int	Account::getTotalAmount(void)
-{
+int	Account::getTotalAmount(void){
 	return (_totalAmount);
 }
 
-int	Account::getNbDeposits(void)
-{
+int	Account::getNbDeposits(void){
 	return (_totalNbDeposits);
 }
 
-int	Account::getNbWithdrawals(void)
-{
+int	Account::getNbWithdrawals(void){
 	return (_totalNbWithdrawals);
+}
+
+void	Account::_displayTimestamp( void ){
+	
+}
+
+void	Account::displayAccountsInfos( void ){
+	_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts << ";";
+	std::cout << "total:" << _totalAmount << ";";
+	std::cout << "deposits:"<< _totalNbDeposits << ";";
+	std::cout << "withdrawals:" << _totalNbWithdrawals << std::endl;
 }
