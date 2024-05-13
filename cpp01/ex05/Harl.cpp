9f5695eeac25b3e0beb7f9c5530db49013905102
AijaRe <aija.repsa@gmail.com>
2024-05-13 22:28:53 +0100

    add tourist Harl

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,17 +6,50 @@
 /*   By: arepsa <arepsa@student.42porto.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 12:39:56 by arepsa            #+#    #+#             */
-/*   Updated: 2024/05/11 12:50:30 by arepsa           ###   ########.fr       */
+/*   Updated: 2024/05/13 22:27:00 by arepsa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
 Harl::Harl( void ) {
-    std::cout << "Harl default constructor called." << std::endl;
+    std::cout << "Harl enters the tourist bus." << std::endl;
 }
 
 Harl::~Harl( void ) {
-    std::cout << "Harl destructor called." << std::endl;
+    std::cout << "Harl leaves the tourist bus." << std::endl;
 }
 
+void    Harl::debug ( void ){
+    std::cout << "I love trips across the country where I can see all the museums, churches and vineyards in one day!" << std::endl;
+}
+
+void    Harl::info ( void ){
+    std::cout << "I can't believe paragliding is not included in my trip cost. I mean, what's a holiday without a bit of adrenaline and a whole lot of screaming?" << std::endl;
+}
+
+void    Harl::warning ( void ){
+    std::cout << "I am quite certain I should be receiving a free massage, I am the most open-minded client you have, you cannot afford losing me!" << std::endl;
+}
+
+void    Harl::error ( void ){
+    std::cout << "Oh no, I think I left my wallet in the winery! This is catastrophic, we need to contact the FBI, CIA, and Interpol to track it down immediately!" << std::endl;
+}
+
+/* 
+** array of function pointers for Harl level private functions
+** define an array of levels and compar them to input level
+*/
+void Harl::complain ( std::string level ) {
+	void(Harl::*harlFunc[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+    for (size_t i = 0; i < 4; i++){
+        if (level == levels[i]) {
+            (this->*harlFunc[i])();
+            return ;
+        }
+    }
+    std::cout << "You are doing an amazing job. Thank you!" << std::endl; //no matching level found
+}
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -10,3 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+void	basic_tests ( void ) {
+	Harl harl;
+	std::cout << "DEBUG level: " << std::endl;
+	harl.complain("DEBUG");
+	std::cout << "INFO level: " << std::endl;
+	harl.complain("INFO");
+	std::cout << "WARNING level: " << std::endl;
+	harl.complain("WARNING");
+	std::cout << "ERROR level: " << std::endl;
+	harl.complain("ERROR");
+}
+
+void	user_test ( char* argv){
+	Harl harl;
+	std::string level = argv;
+	for (size_t i = 0; i < level.length(); i++)
+		level[i] = toupper(level[i]);
+	harl.complain(level);
+}
+
+int		main( int argc, char** argv ) {
+	if (argc == 1){
+		std::cout << "Basic tests.\nYou can try specific test with ./harl <complaint>" << std::endl;
+		basic_tests();
+	}
+	else if (argc == 2)
+		user_test (argv[1]);
+	else
+		std::cout << "Too many arguments." << std::endl;
+
+	return 0;
+}
