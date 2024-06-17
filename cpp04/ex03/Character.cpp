2c95fc22ff31ba618bb94076ae52043ab2b85a3c
AijaRe <aija.repsa@gmail.com>
2024-06-17 19:36:59 +0100

    character basics

--- cpp04/ex03/Character.cpp
+++ cpp04/ex03/Character.cpp
@@ -6,37 +6,81 @@
 /*   By: arepsa <arepsa@student.42porto.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 21:43:56 by arepsa            #+#    #+#             */
-/*   Updated: 2024/06/17 18:04:41 by arepsa           ###   ########.fr       */
+/*   Updated: 2024/06/17 19:36:34 by arepsa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Character.hpp"
+#include "AMateria.hpp"
 
-Character::Character( void ) : _name("nameless") {
+/* 
+** pointers (_inventory) by default are initialized to nullptr
+*/
+Character::Character( void ) : _name("nameless"), _inventory() {
     std::cout << "Character default constructor called." << std::endl;
 }
 
 Character::Character( const Character &src ) : _name(src._name) {
     std::cout << "Character copy constructor called." << std::endl;
-    *this = src;
+     for (int i = 0; i < SLOTS; ++i) {
+        if (src._inventory[i])
+            this->_inventory[i] = src._inventory[i]->clone();
+        else
+            this->_inventory[i] = nullptr;
+    }
 }
 
-Character::Character( std::string name ) : _name(name) {
+Character::Character( std::string name ) : _name(name), _inventory() {
     std::cout << "Character assignment constructor called." << std::endl;
 }
 
 Character::~Character( void ) {
     std::cout << "Character destructor called." << std::endl;
+     for (int i = 0; i < SLOTS; ++i) {
+        if (this->_inventory[i])
+            delete this->_inventory[i];
+    }
 }
 
 Character & Character::operator=( const Character &src ) {
     std::cout << "Character assignment operator called." << std::endl;
     if (this != &src) {
-        this->_name = src._name;
+        if (this->_name != src._name)
+            this->_name = src._name;
+        for (int i = 0; i < SLOTS; ++i) {
+            if (this->_inventory[i])
+                delete this->_inventory[i];
+            if (src._inventory[i])
+                this->_inventory[i] = src._inventory[i]->clone();
+            else //is this else necessary? doesn't delete set pointer to null?
+                this->_inventory[i] = nullptr;
+        }
     }
     return *this;
 }
 
 std::string const & Character::getName( void ) const {
     return this->_name;
+}
+
+void    Character::equip( AMateria* m ) {
+    for (int i = 0; i > SLOTS; i++) {
+        if(!this->_inventory[i])
+            this->_inventory[i] = m;
+            std::cout << this->_name << " equiping " << m->getType() << std::endl;
+            m = NULL; // what do you mean by Assuming ownership is transferred to _inventory?
+            break ;
+    }
+}
+
+void    Character::unequip( int idx ) {
+    if (idx >= 0 && idx < SLOTS && this->_inventory[idx])
+        this->_inventory[idx] = NULL;
+}
+
+void    Character::use( int idx, ICharacter& target ) {
+    if (idx >= 0 && idx < SLOTS && _inventory[idx])
+        _inventory[idx]->use(target);
+        std::cout << "Using " << _inventory[idx]->getType();
+        std::cout << " on " << target.getName() << std::endl;
 }
--- cpp04/ex03/Character.hpp
+++ cpp04/ex03/Character.hpp
@@ -6,27 +6,35 @@
 /*   By: arepsa <arepsa@student.42porto.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 21:43:59 by arepsa            #+#    #+#             */
-/*   Updated: 2024/06/17 18:01:36 by arepsa           ###   ########.fr       */
+/*   Updated: 2024/06/17 19:07:50 by arepsa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHARACTER_HPP
 #define CHARACTER_HPP
 
-#include "ICharacter.hpp"
 #include <iostream>
+#include "ICharacter.hpp"
+
+#define SLOTS 4
+
+class AMateria;
 
 class Character : public ICharacter {
     private:
         std::string _name;
+        AMateria* _inventory[SLOTS];
     public:
         Character( void );
         Character( std::string name );
         Character( const Character &src );
-        ~Character( void );
+        virtual ~Character( void );
         Character & operator=( const Character &src );
 
         std::string const & getName( void ) const;
+        void equip(AMateria* m);
+        void unequip(int idx);
+        void use(int idx, ICharacter& target);
 };
 
 #endif // CHARACTER_HPP
