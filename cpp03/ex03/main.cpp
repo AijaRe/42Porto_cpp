fcd430e5ca760fe7bb1f99237a36aab6bc417e4d
AijaRe <aija.repsa@gmail.com>
2024-10-01 15:45:08 +0100

    add shadow, deep copy test

--- cpp03/ex03/main.cpp
+++ cpp03/ex03/main.cpp
@@ -22,6 +22,11 @@ int	main ( void ) {
     std::cout << "-----DIAMONDTRAP COPY CONSTRUCTOR-----" << std::endl;
     DiamondTrap c(s);
     std::cout << c << std::endl;
+    std::cout << "--------VERIFY DEEP COPY--------" << std::endl;
+    std::cout << "Change s attack damage to 44:" << std::endl;
+    s.setAttackDamage(44);
+     std::cout << "s attack damage: " << s.getAttackDamage() << std::endl;
+    std::cout << "c attack damage: " << c.getAttackDamage() << std::endl;
     std::cout << "-----DIAMONDTRAP ASSIGNMENT CONSTRUCTOR-----" << std::endl;
     DiamondTrap assigned_s("Shine2");
     assigned_s = s;
