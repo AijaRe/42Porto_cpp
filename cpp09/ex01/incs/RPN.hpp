0c1441b076884c362ce3a4b6ee63f56392c400e3
AijaRe <aija.repsa@gmail.com>
2024-12-01 20:07:27 +0000

    start RPN

--- cpp09/ex01/incs/RPN.hpp
+++ cpp09/ex01/incs/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: arepsa <arepsa@student.42porto.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 12:51:02 by arepsa            #+#    #+#             */
-/*   Updated: 2024/12/01 12:51:03 by arepsa           ###   ########.fr       */
+/*   Updated: 2024/12/01 20:05:03 by arepsa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 #define RPN_HPP
 
 #include <iostream>
+#include <stack>
 
 class RPN {
     public:
         RPN(void);
+        RPN(const std::string &input);
         RPN(const RPN &copy);
         ~RPN(void);
         RPN & operator=(const RPN &src);
     private:
-        // Private members
+        std::stack<int> _stack;
 };
 
 #endif // RPN_HPP
--- cpp09/ex01/srcs/main.cpp
+++ cpp09/ex01/srcs/main.cpp
@@ -12,3 +12,26 @@
 
 #include <iostream>
 #include "RPN.hpp"
+
+int	main(int argc, char** argv) {
+
+	if (argc != 2)
+	{
+		std::cerr << "Incorrect input." << std::endl;
+		std::cerr << "Usage: ./RPN \"1 2 3 + -\"" << std::endl;
+		return 1;
+	}
+	try {
+		std::string input = argv[1];
+		
+		if (input.empty())
+			throw std::runtime_error("Error: Empty input.");
+		
+		RPN rpn(input);
+		
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
